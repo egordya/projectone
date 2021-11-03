19b44650d39e2f88d0d601da3075788e303e40c3
Weppan <93657080+Weppan@users.noreply.github.com>
2021-11-03 15:28:32 +0100

    NU är den bättre

--- testing123.java
+++ testing123.java
@@ -1,15 +1,9 @@
 public class testing123 {
 
     public static void main(String[] args) {
-        String message = "This is a test number 2";
+        String message = "This is a test number 4";
 
         System.out.println(message.trim());
         System.out.println(message);
-
-        String message2 = "This is a test number 2";
-
-        System.out.println(message.trim());
-        System.out.println(message2);
-
     }
 }
