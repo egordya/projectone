6c98cb516458da53bda65a8d3186c2dfa69d1bc3
egordya <91674367+egordya@users.noreply.github.com>
2021-11-03 14:27:28 +0100

    Add files via upload

---
+++ tutorial1.java
@@ -0,0 +1,15 @@
+package com.tutorial;
+
+import java.awt.*;
+import java.util.Locale;
+
+
+public class tutorial1 {
+    public static void main(String[] args) {
+        String message = "    HEllo world" + "!!" ;
+
+        System.out.println(message.trim());
+        System.out.println(message);
+    }
+
+}
